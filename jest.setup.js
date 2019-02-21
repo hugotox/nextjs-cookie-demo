9067dd29d36f3e227bb5e44ca7a92ddc06c87e62
Hugo <hpineda@carelinx.com>
2019-02-21 12:06:49 -0800

    fix tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,6 @@
-jest.mock('next/config', () => () => ({ publicRuntimeConfig: { PORT: 3000 } }));
+import { setConfig } from 'next/config';
+import publicRuntimeConfig from './next.config';
+
+setConfig({ publicRuntimeConfig });
+
+// jest.mock('next/config', () => () => ({ publicRuntimeConfig: { PORT: 3000 } }));
