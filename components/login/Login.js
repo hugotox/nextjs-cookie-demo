12a869abf6d62ba04a3ad2a4d207241bb338c279
Hugo <hpineda@carelinx.com>
2019-04-24 16:53:44 -0700

    Updated version and fixed login issue

--- components/login/Login.js
+++ components/login/Login.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Router from 'next/router';
 import { connect } from 'react-redux';
-import { withRouter } from 'next/router';
 import { login } from './actions';
 
 class Login extends Component {
   static isPublic = true;
 
   static propTypes = {
-    router: PropTypes.object.isRequired,
     dispatch: PropTypes.func.isRequired
   };
 
@@ -18,7 +17,7 @@ class Login extends Component {
   };
 
   handleSubmit = e => {
-    const { router } = this.props;
+    const { router } = Router;
     const { query } = router;
     const next = query.next || '/';
     e.preventDefault();
@@ -35,7 +34,7 @@ class Login extends Component {
       <div className="container">
         <div className="box">
           <h3>Login</h3>
-          <form onSubmit={this.handleSubmit}>
+          <form onSubmit={this.handleSubmit} method="POST">
             <label htmlFor="username">Username</label>
             <input
               id="username"
@@ -87,4 +86,4 @@ const mapStateToProps = state => {
   };
 };
 
-export default withRouter(connect(mapStateToProps)(Login));
+export default connect(mapStateToProps)(Login);
