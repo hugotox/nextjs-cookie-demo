c3558436ac0afbdedae85d5a54ce68c847ebc4f9
Hugo Pineda <hpineda83@gmail.com>
2019-06-26 08:19:45 -0700

    - home page useState hook - rollback eslint (conflicted with prettier)

--- components/index/Home.js
+++ components/index/Home.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import { connect } from 'react-redux';
@@ -6,46 +6,41 @@ import { logout } from '../login/actions';
 import Modal from '../ui/modal/Modal';
 import ModalHeader from '../ui/modal/ModalHeader';
 
-class Home extends Component {
-  static propTypes = {
-    dispatch: PropTypes.func.isRequired,
-    user: PropTypes.object
-  };
+const Home = ({ dispatch, user }) => {
+  const [showModal, setShowModal] = useState(false);
 
-  state = {
-    showModal: false
-  };
-
-  handleLogout = e => {
+  const handleLogout = e => {
     e.preventDefault();
-    this.props.dispatch(logout());
+    dispatch(logout());
   };
 
-  render() {
-    const { user } = this.props;
-    if (!user) {
-      return <div>No user found</div>;
-    }
-    return (
-      <div className="container">
-        <h1>Hi {user.username}</h1>
-        <h2>A dead simple, responsive boilerplate.</h2>
-        <a href="/api/logout" onClick={this.handleLogout}>
-          Logout
-        </a>
-        <br />
-        <Link href="/private">
-          <a>Private</a>
-        </Link>
-        <br />
-        <button onClick={() => this.setState({ showModal: true })}>Show modal</button>
-        <Modal onHide={() => this.setState({ showModal: false })} visible={this.state.showModal}>
-          <ModalHeader>With header</ModalHeader>
-          Hola
-        </Modal>
-      </div>
-    );
+  if (!user) {
+    return <div>No user found</div>;
   }
-}
+  return (
+    <div className="container">
+      <h1>Hi {user.username}</h1>
+      <h2>A dead simple, responsive boilerplate.</h2>
+      <a href="/api/logout" onClick={handleLogout}>
+        Logout
+      </a>
+      <br />
+      <Link href="/private">
+        <a>Private</a>
+      </Link>
+      <br />
+      <button onClick={() => setShowModal(true)}>Show modal</button>
+      <Modal onHide={() => setShowModal(false)} visible={showModal}>
+        <ModalHeader>With header</ModalHeader>
+        Hello!
+      </Modal>
+    </div>
+  );
+};
+
+Home.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  user: PropTypes.object
+};
 
 export default connect(state => state.auth)(Home);
