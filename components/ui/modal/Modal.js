5f5024a65994b70a2dbf8bfcc95a0683a1bf5ba1
Hugo Pineda <hpineda83@gmail.com>
2019-07-07 16:20:50 -0400

    moved modal to hooks

--- components/ui/modal/Modal.js
+++ components/ui/modal/Modal.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-noninteractive-element-interactions */
-import React, { Component } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import ModalHeader from './ModalHeader';
 import { SCREEN_SIZE } from '../../../styles/global-styles';
@@ -7,146 +7,132 @@ import { SCREEN_SIZE } from '../../../styles/global-styles';
 const modalHeaderType = <ModalHeader />.type;
 const animationSpeed = 300;
 
-class Modal extends Component {
-  static propTypes = {
-    visible: PropTypes.bool,
-    onHide: PropTypes.func.isRequired,
-    size: PropTypes.oneOf(['full', 'large', 'medium', 'small']),
-    children: PropTypes.any
-  };
+const Modal = ({ children, onHide, size, visible }) => {
+  const overlayRef = useRef(null);
+  const [fadeIn, setFadeIn] = useState(false);
+  const [display, setDisplay] = useState('none');
 
-  static defaultProps = {
-    size: 'medium'
-  };
-
-  constructor(props) {
-    super(props);
-    this.overlayRef = React.createRef();
-  }
-
-  state = {
-    fadeIn: false,
-    display: 'none'
-  };
-
-  componentDidMount() {
-    window.addEventListener('keydown', this.keyboardListener);
-    this.overlayRef.current.addEventListener('click', this.props.onHide);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (!this.props.visible && nextProps.visible) {
-      this.setState({ display: 'block' });
-      setTimeout(() => {
-        this.setState({ fadeIn: true });
-        document.body.style.overflow = 'hidden';
-      }, 10);
-    }
-    if (this.props.visible && !nextProps.visible) {
-      this.setState({ fadeIn: false });
-      setTimeout(() => {
-        this.setState({ display: 'none' });
-        document.body.style.overflow = '';
-      }, animationSpeed);
-    }
-  }
-
-  componentWillUnmount() {
-    if (typeof window !== 'undefined') {
-      window.removeEventListener('keydown', this.keyboardListener);
-      this.overlayRef.current.removeEventListener('click', this.props.onHide);
-    }
-  }
-
-  keyboardListener = e => {
+  const keyboardListener = e => {
     const key = e.key || e.keyCode;
     if (key === 'Escape' || key === 'Esc' || key === 27) {
-      const { onHide, visible } = this.props;
       if (visible) {
         onHide();
       }
     }
   };
 
-  render() {
-    const { children, onHide, size } = this.props;
-    const childrenWithProps = React.Children.map(children, child => {
-      if (child.type === modalHeaderType) {
-        return React.cloneElement(child, { onHide });
-      } else {
-        return child;
-      }
-    });
-    let width;
-    switch (size) {
-      case 'full':
-        width = '100%';
-        break;
+  useEffect(() => {
+    window.addEventListener('keydown', keyboardListener);
+    overlayRef.current.addEventListener('click', onHide);
 
-      case 'large':
-        width = '80%';
-        break;
-
-      case 'medium':
-        width = '600px';
-        break;
-
-      case 'small':
-        width = '400px';
-        break;
+    if (visible) {
+      setDisplay('block');
+      setTimeout(() => {
+        setFadeIn(true);
+        document.body.style.overflow = 'hidden';
+      }, 50);
+    } else {
+      setFadeIn(false);
+      setTimeout(() => {
+        setDisplay('none');
+        document.body.style.overflow = '';
+      }, animationSpeed);
     }
-    return (
-      <div className="wrapper" style={{ display: this.state.display }}>
-        <div className="modal-overlay" ref={this.overlayRef} />
-        <div className={'box xmodal ' + (this.state.fadeIn ? 'fadeIn' : '')} data-testid="modal-body">
-          {childrenWithProps}
-        </div>
-        <style jsx>{/*language=CSS*/
-        `
-          .wrapper {
-            position: fixed;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-          }
 
-          .modal-overlay {
-            position: fixed;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-            background-color: #999999;
-            opacity: 0.3;
-          }
+    return () => {
+      window.removeEventListener('keydown', keyboardListener);
+      overlayRef.current.removeEventListener('click', onHide);
+    };
+  }, [visible]);
 
-          .xmodal {
-            position: ${size === 'full' ? 'fixed' : 'relative'};
-            width: ${width};
-            min-height: 100px;
-            margin: 0 auto;
-            top: -10%;
-            bottom: ${size === 'full' ? '0' : 'auto'};
-            opacity: 0;
-            transition: opacity ${animationSpeed}ms ease-in-out, top ${animationSpeed}ms ease-in-out;
-          }
+  const childrenWithProps = React.Children.map(children, child => {
+    if (child.type === modalHeaderType) {
+      return React.cloneElement(child, { onHide });
+    } else {
+      return child;
+    }
+  });
 
-          .xmodal.fadeIn {
-            opacity: 1;
-            top: ${size === 'full' ? '0' : '10%'};
-            bottom: ${size === 'full' ? '0' : 'auto'};
-          }
+  let width;
+  switch (size) {
+    case 'full':
+      width = '100%';
+      break;
 
-          @media screen and (max-width: ${SCREEN_SIZE.small}) {
-            .xmodal {
-              width: 90%;
-            }
-          }
-        `}</style>
-      </div>
-    );
+    case 'large':
+      width = '80%';
+      break;
+
+    case 'medium':
+      width = '600px';
+      break;
+
+    case 'small':
+      width = '400px';
+      break;
   }
-}
+
+  return (
+    <div className="wrapper" style={{ display }}>
+      <div className="modal-overlay" ref={overlayRef} />
+      <div className={'box xmodal ' + (fadeIn ? 'fadeIn' : '')} data-testid="modal-body">
+        {childrenWithProps}
+      </div>
+      <style jsx>{`
+        .wrapper {
+          position: fixed;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
+        }
+
+        .modal-overlay {
+          position: fixed;
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
+          background-color: #999999;
+          opacity: 0.3;
+        }
+
+        .xmodal {
+          position: ${size === 'full' ? 'fixed' : 'relative'};
+          width: ${width};
+          min-height: 100px;
+          margin: 0 auto;
+          top: -10%;
+          bottom: ${size === 'full' ? '0' : 'auto'};
+          opacity: 0;
+          transition: opacity ${animationSpeed}ms ease-in-out, top ${animationSpeed}ms ease-in-out;
+        }
+
+        .xmodal.fadeIn {
+          opacity: 1;
+          top: ${size === 'full' ? '0' : '10%'};
+          bottom: ${size === 'full' ? '0' : 'auto'};
+        }
+
+        @media screen and (max-width: ${SCREEN_SIZE.small}) {
+          .xmodal {
+            width: 90%;
+          }
+        }
+      `}</style>
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  visible: PropTypes.bool,
+  onHide: PropTypes.func.isRequired,
+  size: PropTypes.oneOf(['full', 'large', 'medium', 'small']),
+  children: PropTypes.any
+};
+
+Modal.defaultProps = {
+  size: 'medium'
+};
 
 export default Modal;
