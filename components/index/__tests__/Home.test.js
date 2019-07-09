50c2552cf7ab138172e4517bcacbe25257cbb0d5
Hugo Pineda <hpineda83@gmail.com>
2019-07-09 16:24:04 -0400

    Fixed tests

--- components/index/__tests__/Home.test.js
+++ components/index/__tests__/Home.test.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { cleanup } from '@testing-library/react';
-import renderWithRedux from '../../../lib/test-utils/render-with-redux';
+import { render, cleanup } from '@testing-library/react';
 import Home from '../Home';
 
 describe('Index page tests', () => {
   afterEach(cleanup);
 
   it('should render', function() {
-    const { getByText } = renderWithRedux(<Home />, {
-      initialState: {
-        auth: {
-          user: { username: 'Test' }
-        }
-      }
-    });
+    const { getByText } = render(<Home user={{ username: 'Test' }} logout={() => {}} />);
     expect(getByText('A dead simple, responsive boilerplate.')).not.toBe(null);
   });
 });
